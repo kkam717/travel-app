63af301ec7714081b6cdd6fce3837960c85f207a
kkam717 <kian>
2026-01-30 21:52:47 +0000

    Enable map drag and zoom gestures inside ListView

--- lib/widgets/itinerary_map.dart
+++ lib/widgets/itinerary_map.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../models/itinerary.dart';
@@ -110,6 +112,9 @@ class _ItineraryMapState extends State<ItineraryMap> {
           onMapCreated: (ctrl) {
             _controller.complete(ctrl);
             if (mounted) _fitBounds();
+          },
+          gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+            Factory<OneSequenceGestureRecognizer>(() => EagerGestureRecognizer()),
           },
           myLocationButtonEnabled: true,
           zoomControlsEnabled: true,
